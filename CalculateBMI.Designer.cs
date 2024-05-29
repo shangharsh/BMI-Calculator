7db856bd93aecb045749f934e314cddb604f672d
Shangharsh Adhikari <san.adh1996@gmail.com>
2024-05-29 15:52:08 +0545

    feat: Calculates BMI

---
+++ CalculateBMI.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace BMICalculator
+{
+    partial class CalculateBMI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtWeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtHeight = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TxtStatus = new System.Windows.Forms.TextBox();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.ComboUnit = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Weight";
+            // 
+            // TxtWeight
+            // 
+            this.TxtWeight.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtWeight.Location = new System.Drawing.Point(19, 175);
+            this.TxtWeight.Name = "TxtWeight";
+            this.TxtWeight.Size = new System.Drawing.Size(196, 32);
+            this.TxtWeight.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(248, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Height";
+            // 
+            // TxtHeight
+            // 
+            this.TxtHeight.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtHeight.Location = new System.Drawing.Point(252, 175);
+            this.TxtHeight.Name = "TxtHeight";
+            this.TxtHeight.Size = new System.Drawing.Size(196, 32);
+            this.TxtHeight.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TxtStatus);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.BtnClear);
+            this.groupBox1.Controls.Add(this.TxtWeight);
+            this.groupBox1.Controls.Add(this.BtnCalculate);
+            this.groupBox1.Controls.Add(this.TxtHeight);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ComboUnit);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.TxtResult);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(92, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 372);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Calculate BMI";
+            // 
+            // TxtStatus
+            // 
+            this.TxtStatus.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtStatus.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtStatus.ForeColor = System.Drawing.Color.Red;
+            this.TxtStatus.Location = new System.Drawing.Point(252, 213);
+            this.TxtStatus.Name = "TxtStatus";
+            this.TxtStatus.ReadOnly = true;
+            this.TxtStatus.Size = new System.Drawing.Size(196, 25);
+            this.TxtStatus.TabIndex = 4;
+            this.TxtStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.AutoSize = true;
+            this.BtnClear.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClear.Location = new System.Drawing.Point(315, 304);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(133, 33);
+            this.BtnClear.TabIndex = 5;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.AutoSize = true;
+            this.BtnCalculate.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalculate.Location = new System.Drawing.Point(18, 304);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(133, 33);
+            this.BtnCalculate.TabIndex = 4;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // ComboUnit
+            // 
+            this.ComboUnit.Items.AddRange(new object[] {
+            "Imperial System(Pound/Inches)",
+            "Metric System(Kg/m)"});
+            this.ComboUnit.Location = new System.Drawing.Point(18, 85);
+            this.ComboUnit.Name = "ComboUnit";
+            this.ComboUnit.Size = new System.Drawing.Size(429, 38);
+            this.ComboUnit.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Unit:";
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Enabled = false;
+            this.TxtResult.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtResult.Location = new System.Drawing.Point(252, 251);
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.ReadOnly = true;
+            this.TxtResult.Size = new System.Drawing.Size(196, 32);
+            this.TxtResult.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(179, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Result:";
+            // 
+            // CalculateBMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 517);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CalculateBMI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CalculateBMI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtWeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtHeight;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ComboUnit;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtStatus;
+    }
+}
---
+++ CalculateBMI.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMICalculator
+{
+    public partial class CalculateBMI : Form
+    {
+        public CalculateBMI()
+        {
+            InitializeComponent();
+        }
+
+        void Calculate()
+        {
+
+            if ( TxtHeight.TextLength>0 && TxtWeight.TextLength > 0 && ComboUnit.SelectedIndex == 0)
+            {
+                //MessageBox.Show("Pound");
+                //
+                double bmi, weight, height;
+                weight = double.Parse(TxtWeight.Text.ToString());
+                height = double.Parse(TxtHeight.Text.ToString());
+
+                bmi = 703*weight / (height * height);
+                if (bmi < 18.5)
+                {
+                    //TxtResult.BackColor = Color.Yellow;
+                    TxtStatus.BackColor = Color.Yellow;
+                    TxtStatus.Text = "UnderWeight";
+                    TxtStatus.ForeColor = Color.White;
+
+                }
+                else if (bmi > 18.5 && bmi < 25)
+                {
+                    //TxtResult.BackColor = Color.Green;
+                    TxtStatus.BackColor = Color.Green;
+                    TxtStatus.Text = "HealthyWeight";
+                    TxtStatus.ForeColor = Color.White;
+
+                }
+                else if (bmi > 25 && bmi < 30)
+                {
+                    //TxtResult.BackColor = Color.Orange;
+                    TxtStatus.BackColor = Color.Orange;
+                    TxtStatus.Text = "OverWeight";
+                    TxtStatus.ForeColor = Color.White;
+
+                }
+                else if (bmi > 30)
+                {
+                    //TxtResult.BackColor = Color.Red;
+                    TxtStatus.BackColor = Color.Red;
+                    TxtStatus.Text = "Obesity";
+                    TxtStatus.ForeColor = Color.White;
+
+                }
+                TxtResult.Text = bmi.ToString();
+                //MessageBox.Show(bmi.ToString());
+                //Math.Round(bmi * 10 ^ x) / 10 ^ x
+
+            }
+            else if (TxtHeight.TextLength > 0 && TxtWeight.TextLength > 0 && ComboUnit.SelectedIndex == 1)
+            {
+                //MessageBox.Show("Kg");
+                //Calculate();
+                double bmi, weight, height;
+                weight = double.Parse(TxtWeight.Text.ToString());
+                height = double.Parse(TxtHeight.Text.ToString());
+
+                bmi = weight / (height * height);
+                if (bmi < 18.5)
+                {
+                    //TxtResult.BackColor = Color.Yellow;
+                    TxtStatus.BackColor = Color.Yellow;
+                    TxtStatus.Text = "UnderWeight";
+                    TxtStatus.ForeColor = Color.White;
+                }
+                else if (bmi > 18.5 && bmi < 25)
+                {
+                    //TxtResult.BackColor = Color.Green;
+                    TxtStatus.BackColor = Color.Green;
+                    TxtStatus.Text = "HealthyWeight";
+                    TxtStatus.ForeColor = Color.White;
+
+                }
+                else if (bmi > 25 && bmi < 30)
+                {
+                    //TxtResult.BackColor = Color.Orange;
+                    TxtStatus.BackColor = Color.Orange;
+                    TxtStatus.Text = "OverWeight";
+                    TxtStatus.ForeColor = Color.White;
+
+                }
+                else if (bmi > 30)
+                {
+                    //TxtResult.BackColor = Color.Red;
+                    TxtStatus.BackColor = Color.Red;
+                    TxtStatus.Text = "Obesity";
+                    TxtStatus.ForeColor = Color.White;
+
+                }
+                TxtResult.Text = bmi.ToString();
+                //MessageBox.Show(bmi.ToString());
+                //Math.Round(bmi * 10 ^ x) / 10 ^ x
+            }
+            else
+            {
+                MessageBox.Show("Please Select Option From Unit List");
+            }
+
+
+
+
+
+            
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+           Calculate();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            TxtWeight.Clear();
+            TxtHeight.Clear();
+            ComboUnit.SelectedIndex = -1;
+            TxtResult.Clear();
+            TxtStatus.Clear();
+        }
+
+        
+    }
+}
